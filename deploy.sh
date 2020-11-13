3dcad69257b3af4b2f570136538e898608dc8fba
Kota Uchida <uchida@c.titech.ac.jp>
2020-11-13 18:50:28 +0900

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+target_branch="ghp-deploy"
+repo_uri="https://${GH_TOKEN}@github.com/uchan-nos/myproj.git"
+
+git config user.name "CircleCI deployer"
+git config user.mail "<>"
+git checkout $target_branch
+git reset --hard origin/master
+
+gcc -o a.out a.c
+echo "size of a.out is $(wc -c a.out)" > a.attr
+
+git add a.out a.attr
+git commit -m "[skip ci] updates GitHub Pages"
+if [ $? -ne 0 ]; then
+  echo "nothing to commit"
+  exit 0
+fi
+
+git remote set-url origin $repo_uri
+git push -f origin $target_branch
