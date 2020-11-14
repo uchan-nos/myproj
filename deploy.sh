2e8ac20bbdf06ce63c882bf024cb799eace71f98
Kota UCHIDA <uchan0@gmail.com>
2020-11-14 11:04:29 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,17 @@
 #!/bin/sh -ex
-
 target_branch="ghp-deploy"
-repo_uri="git@github.com:uchan-nos/myproj.git"
-
-git config user.name "CircleCI deployer"
-git config user.email "<>"
+git config --global user.name "CircleCI deployer"
+git config --global user.email "<>"
 git checkout $target_branch
-git reset --hard origin/main
 
 gcc -o a.out a.c
-echo "size of a.out is $(wc -c a.out)" > a.attr
+echo "size of a.out is $(wc -c a.out)" > a.txt
 
-git add a.out a.attr
+git add a.out a.txt
 git commit -m "[skip ci] updates GitHub Pages"
 if [ $? -ne 0 ]; then
   echo "nothing to commit"
   exit 0
 fi
-
-git remote set-url origin $repo_uri
+git remote set-url origin "git@github.com:uchan-nos/myproj.git"
 git push -f origin $target_branch
